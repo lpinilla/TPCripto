0dd5aabfd3d51096597687f2444cfdc55403ea26
Micaela Banfi <mbanfi@itba.edu.ar>
2020-05-29 21:34:34 -0300

    test lsb4 encriptado

--- tests/general_test.c
+++ tests/general_test.c
@@ -22,24 +22,55 @@ void lsb4_insert_3();
 void lsb1_insert_image();
 void lsb4_insert_image();
 void lsb4_insert();
+void ladoLSB4aes256ofb();
 
 int main()
 {
     create_suite("General Test");
-    //add_test(lsb_1);
-    //add_test(lsb_4);
-    add_test(lsb1_insert);
-    add_test(lsb4_insert);
+    add_test(lsb_1);
+    add_test(lsb_4);
+     add_test(lsb1_insert);
+     add_test(lsb4_insert);
     add_test(lsb1_insert_2);
     add_test(lsb1_insert_3);
     add_test(lsb4_insert_3);
     add_test(lsb1_insert_image);
     add_test(lsb4_insert_image);
+    add_test(ladoLSB4aes256ofb);
 
     //add_test(lsb_1_4_test(1));
 
     run_suite();
     clear_suite();
+}
+
+void ladoLSB4aes256ofb()
+{
+
+    bmp_file bmp_f = read_bmp("ladoLSB4aes256ofb.bmp");
+    bmp_header bmp_h = bmp_f->header;
+    carrier c = create_carrier(bmp_f->data, bmp_h->image_size_bytes, bmp_h->width_px, bmp_h->height_px);
+
+    lsb l = create_lsb(4);
+
+    payload p = extract_payload(l, c);
+
+    uint8_t *plaintext = malloc(p->size);
+    printf("HOLA\n");
+    
+    int d = decrypt(plaintext, "secreto", p->content, p->size, ofb, aes256);
+    printf(" size: %ld d: %d\n ",p->size,d);
+    FILE *f = fopen("ladoLSB4aes256ofb_prueba.png", "w");
+
+    //muy importante el +4 para saltear el tamanio de lo encriptado
+    fwrite(plaintext+4, sizeof(uint8_t), d, f);
+    fclose(f);
+    destroy_lsb(l);
+    destroy_carrier(c);
+    destroy_payload(p);
+    assert_true(1 == 1);
+
+    
 }
 
 void lsb_1()
